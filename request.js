09779f4ecaf9084b076fd771c17b08fa4d8e4ddc
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-02-06 01:32:14 +0300

    xmhhttprequest kullanılması için new xmlhttprequest nesnesi oluşturuldu.

--- request.js
+++ request.js
@@ -3,4 +3,14 @@ document.querySelector("#getUsers").addEventListener('click',getUsers)
 
 function getUsers() {
     
+    // xmlhttprequest kullanılarak veri çekmesi için yeni xmlhttprequest nesnesi oluşturuldu.
+    const xhr = new XMLHttpRequest()
+    
+    //"https://jsonplaceholder.typicode.com/users" apisinden get metodu ile veriyi alması için open özelliğ kulllanıldı
+    xhr.open("GET","https://jsonplaceholder.typicode.com/users",true)
+    
+    // sayfada herhangi bir değişiklik olduğunda yani sunucudan bilgi istendiğinde tetiklenmesi için onreadystatechange özelliği kullanıldı
+    xhr.onreadystatechange = function () {
+    
+    }
 }
